7f94318170b5b29e67ed29d4ed2232adcf72de4c
Kaan Ozkan <kaanfsd@gmail.com>
2023-09-13 17:51:19 +0300

    Add project files.

---
+++ EcomProjectMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcomProjectMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
