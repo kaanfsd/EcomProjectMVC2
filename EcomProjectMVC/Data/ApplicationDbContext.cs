0b1e573d5a8b254b92aebcfee9dcef9d1e3b85ca
Kaan Ozkan <kaanfsd@gmail.com>
2023-09-14 13:28:41 +0300

    14th September 2023 * Created Categories table.

---
+++ EcomProjectMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EcomProjectMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcomProjectMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
