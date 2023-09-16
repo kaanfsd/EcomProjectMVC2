1ce8255e167c17e98640bc97d3d39811bf8ca47d
Kaan Ozkan <kaanfsd@gmail.com>
2023-09-16 19:43:01 +0300

    16th September 2023 * Added Product model * Pushed product to database * Implemented product repository * Configured product repositrory in UnitOfWork * Created product controller and methods of it * Created views for Create, Edit, Delete and Index * Added Client & Server side validations.

--- Ecom.DataAccess/Data/ApplicationDbContext.cs
+++ Ecom.DataAccess/Data/ApplicationDbContext.cs
@@ -11,12 +11,137 @@ namespace EcomProjectMVC.DataAccess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Smartphone",
+                    Description = "High-performance smartphone with 6.4-inch display.",
+                    Barcode = "SMP12345",
+                    Manufacturer = "Samsung",
+                    ListPrice = 499.99,
+                    Price = 449.99,
+                    Price50 = 429.99,
+                    Price100 = 399.99
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Laptop",
+                    Description = "Thin and light laptop with Intel Core i7 processor.",
+                    Barcode = "LTPT67890",
+                    Manufacturer = "Dell",
+                    ListPrice = 1099.99,
+                    Price = 999.99,
+                    Price50 = 949.99,
+                    Price100 = 899.99
+                },
+                new Product
+                {
+                    Id = 3,
+                    Title = "LED TV",
+                    Description = "55-inch 4K Ultra HD Smart LED TV with HDR.",
+                    Barcode = "TV1234",
+                    Manufacturer = "Sony",
+                    ListPrice = 799.99,
+                    Price = 699.99,
+                    Price50 = 649.99,
+                    Price100 = 599.99
+                },
+                new Product
+                {
+                    Id = 4,
+                    Title = "Wireless Earbuds",
+                    Description = "Bluetooth wireless earbuds with noise cancellation.",
+                    Barcode = "EB5678",
+                    Manufacturer = "Apple",
+                    ListPrice = 199.99,
+                    Price = 179.99,
+                    Price50 = 169.99,
+                    Price100 = 159.99
+                },
+                new Product
+                {
+                    Id = 5,
+                    Title = "Digital Camera",
+                    Description = "20MP digital camera with 3-inch LCD screen.",
+                    Barcode = "CAM4567",
+                    Manufacturer = "Canon",
+                    ListPrice = 299.99,
+                    Price = 249.99,
+                    Price50 = 239.99,
+                    Price100 = 229.99
+                },
+                new Product
+                {
+                    Id = 6,
+                    Title = "Coffee Maker",
+                    Description = "Programmable coffee maker with built-in grinder.",
+                    Barcode = "CM7890",
+                    Manufacturer = "Breville",
+                    ListPrice = 129.99,
+                    Price = 109.99,
+                    Price50 = 99.99,
+                    Price100 = 89.99
+                },
+                new Product
+                {
+                    Id = 7,
+                    Title = "Fitness Tracker",
+                    Description = "Waterproof fitness tracker with heart rate monitor.",
+                    Barcode = "FIT123",
+                    Manufacturer = "Fitbit",
+                    ListPrice = 79.99,
+                    Price = 69.99,
+                    Price50 = 64.99,
+                    Price100 = 59.99
+                },
+                new Product
+                {
+                    Id = 8,
+                    Title = "Backpack",
+                    Description = "Durable backpack with multiple compartments.",
+                    Barcode = "BP5678",
+                    Manufacturer = "North Face",
+                    ListPrice = 59.99,
+                    Price = 49.99,
+                    Price50 = 47.99,
+                    Price100 = 44.99
+                },
+                new Product
+                {
+                    Id = 9,
+                    Title = "Desk Chair",
+                    Description = "Ergonomic office chair with lumbar support.",
+                    Barcode = "CHAIR999",
+                    Manufacturer = "Herman Miller",
+                    ListPrice = 399.99,
+                    Price = 349.99,
+                    Price50 = 329.99,
+                    Price100 = 299.99
+                },
+                new Product
+                {
+                    Id = 10,
+                    Title = "Sneakers",
+                    Description = "Running sneakers with cushioned soles.",
+                    Barcode = "SNK1234",
+                    Manufacturer = "Nike",
+                    ListPrice = 129.99,
+                    Price = 119.99,
+                    Price50 = 109.99,
+                    Price100 = 99.99
+                }
+
                 );
         }
     }
---
+++ Ecom.DataAccess/Migrations/20230916163008_addProductToDb.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EcomProjectMVC.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addProductToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Barcode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Manufacturer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListPrice = table.Column<double>(type: "float", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Price50 = table.Column<double>(type: "float", nullable: false),
+                    Price100 = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Barcode", "Description", "ListPrice", "Manufacturer", "Price", "Price100", "Price50", "Title" },
+                values: new object[,]
+                {
+                    { 1, "SMP12345", "High-performance smartphone with 6.4-inch display.", 499.99000000000001, "Samsung", 449.99000000000001, 399.99000000000001, 429.99000000000001, "Smartphone" },
+                    { 2, "LTPT67890", "Thin and light laptop with Intel Core i7 processor.", 1099.99, "Dell", 999.99000000000001, 899.99000000000001, 949.99000000000001, "Laptop" },
+                    { 3, "TV1234", "55-inch 4K Ultra HD Smart LED TV with HDR.", 799.99000000000001, "Sony", 699.99000000000001, 599.99000000000001, 649.99000000000001, "LED TV" },
+                    { 4, "EB5678", "Bluetooth wireless earbuds with noise cancellation.", 199.99000000000001, "Apple", 179.99000000000001, 159.99000000000001, 169.99000000000001, "Wireless Earbuds" },
+                    { 5, "CAM4567", "20MP digital camera with 3-inch LCD screen.", 299.99000000000001, "Canon", 249.99000000000001, 229.99000000000001, 239.99000000000001, "Digital Camera" },
+                    { 6, "CM7890", "Programmable coffee maker with built-in grinder.", 129.99000000000001, "Breville", 109.98999999999999, 89.989999999999995, 99.989999999999995, "Coffee Maker" },
+                    { 7, "FIT123", "Waterproof fitness tracker with heart rate monitor.", 79.989999999999995, "Fitbit", 69.989999999999995, 59.990000000000002, 64.989999999999995, "Fitness Tracker" },
+                    { 8, "BP5678", "Durable backpack with multiple compartments.", 59.990000000000002, "North Face", 49.990000000000002, 44.990000000000002, 47.990000000000002, "Backpack" },
+                    { 9, "CHAIR999", "Ergonomic office chair with lumbar support.", 399.99000000000001, "Herman Miller", 349.99000000000001, 299.99000000000001, 329.99000000000001, "Desk Chair" },
+                    { 10, "SNK1234", "Running sneakers with cushioned soles.", 129.99000000000001, "Nike", 119.98999999999999, 99.989999999999995, 109.98999999999999, "Sneakers" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+        }
+    }
+}
