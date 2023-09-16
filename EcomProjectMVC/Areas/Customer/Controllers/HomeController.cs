17b03553ad658f0ae87f25085acb24bc778e023c
Kaan Ozkan <kaanfsd@gmail.com>
2023-09-16 13:36:36 +0300

    16th September 2023 * Added IRepository interface * Added ICategoryRepository interface * Implemented UnitOfWork * Added Admin and Customer areas * Added dropdown for content management.

--- EcomProjectMVC/Areas/Customer/Controllers/HomeController.cs
+++ EcomProjectMVC/Areas/Customer/Controllers/HomeController.cs
@@ -2,8 +2,9 @@ using Ecom.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace EcomProjectMVC.Controllers
+namespace EcomProjectMVC.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- EcomProjectMVC/Program.cs
+++ EcomProjectMVC/Program.cs
@@ -1,3 +1,5 @@
+using Ecom.DataAccess.Repository;
+using Ecom.DataAccess.Repository.IRepository;
 using EcomProjectMVC.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 
 
@@ -29,6 +33,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
